63ba567b62bd105163a7e3ef84ab34c65f9d33d6
Volodymyr <vl.zinchenko@gmail.com>
2023-11-14 22:55:12 -0500

    Chapter 10 Partial Prerendering

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,14 +1,23 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-export default async function LatestInvoices() {
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
+import Image from 'next/image'
+import { lusitana } from '@/app/ui/fonts'
+import { fetchLatestInvoices } from '@/app/lib/data'
+
+export default async function LatestInvoices () {
+  const latestInvoices = await fetchLatestInvoices()
+
+  if (!latestInvoices || latestInvoices.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>
+  }
+
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+      <div
+        className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
@@ -27,6 +36,7 @@ export default async function LatestInvoices() {
                     className="mr-4 rounded-full"
                     width={32}
                     height={32}
+                    alt=""
                   />
                   <div className="min-w-0">
                     <p className="truncate text-sm font-semibold md:text-base">
@@ -43,14 +53,14 @@ export default async function LatestInvoices() {
                   {invoice.amount}
                 </p>
               </div>
-            );
+            )
           })}
         </div>
         <div className="flex items-center pb-2 pt-6">
-          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+          <ArrowPathIcon className="h-5 w-5 text-gray-500"/>
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
       </div>
     </div>
-  );
+  )
 }
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,7 @@
-import { generateYAxis } from '@/app/lib/utils';
-import { CalendarIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { generateYAxis } from '@/app/lib/utils'
+import { CalendarIcon } from '@heroicons/react/24/outline'
+import { lusitana } from '@/app/ui/fonts'
+import { fetchRevenue } from '@/app/lib/data'
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -8,13 +9,15 @@ import { lusitana } from '@/app/ui/fonts';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart() {
-  const chartHeight = 350;
-  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+export default async function RevenueChart () {
+  const revenue = await fetchRevenue(); // Fetch data inside the component
 
   if (!revenue || revenue.length === 0) {
-    return <p className="mt-4 text-gray-400">No data available.</p>;
+    return <p className="mt-4 text-gray-400">No data available.</p>
   }
+
+  const chartHeight = 350
+  const { yAxisLabels, topLabel } = generateYAxis(revenue)
 
   return (
     <div className="w-full md:col-span-4">
@@ -22,7 +25,8 @@ export default async function RevenueChart() {
         Recent Revenue
       </h2>
       <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+        <div
+          className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
           {/* y-axis */}
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
@@ -50,10 +54,10 @@ export default async function RevenueChart() {
           ))}
         </div>
         <div className="flex items-center pb-2 pt-6">
-          <CalendarIcon className="h-5 w-5 text-gray-500" />
+          <CalendarIcon className="h-5 w-5 text-gray-500"/>
           <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
         </div>
       </div>
     </div>
-  );
+  )
 }
