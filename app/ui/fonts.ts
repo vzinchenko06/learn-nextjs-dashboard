b14e7b919964f987c957b40ad46ddbc9adff5cac
Volodymyr <vl.zinchenko@gmail.com>
2023-11-14 19:50:22 -0500

    Chapter 6. Create a repo and Vercel account, setting up database

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, Noto_Sans_Symbols_2, Ubuntu } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
+
+export const ubuntu = Ubuntu({ subsets: ['latin'], weight: ['400', '700'] })
+
