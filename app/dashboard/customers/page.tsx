14bdfb9823fab60ba079bc388448e50c3ce4131b
Volodymyr <vl.zinchenko@gmail.com>
2023-11-15 18:50:40 -0500

    Chapter 16. Adding Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,13 @@
-import { fetchFilteredCustomers } from '@/app/lib/data'
 import CustomersTable from '@/app/ui/customers/table'
 import { Metadata } from 'next'
+import { lusitana } from '@/app/ui/fonts'
+import Search from '@/app/ui/search'
+import { Suspense } from 'react'
+import { TableSkeleton } from '@/app/ui/dashboard/skeletons'
+
 export const metadata: Metadata = {
   title: 'Customers',
-};
+}
 
 type PageProps = {
   searchParams?: {
@@ -15,9 +19,23 @@ export default async function Page ({
   searchParams,
 }: PageProps) {
   const query = searchParams?.query || ''
-  const customers = await fetchFilteredCustomers(query)
 
   return (
-    <CustomersTable customers={customers}/>
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <div className="mt-4">
+        <Search placeholder="Search customers..."/>
+      </div>
+      <Suspense fallback={<TableSkeleton cols={[
+        'Name',
+        'Email',
+        'Total Invoices',
+        'Total Pending',
+        'Total Paid']}/>}>
+        <CustomersTable query={query}/>
+      </Suspense>
+    </div>
   )
 }
