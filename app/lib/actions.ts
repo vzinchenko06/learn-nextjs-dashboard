b64ee700e7a3c0ce532634029f8106f377eafc0d
Volodymyr <vl.zinchenko@gmail.com>
2023-11-15 17:21:52 -0500

    Chapter 15. Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,21 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from '@/auth'
+
+export async function authenticate (
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData))
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'InvalidCredential'
+    }
+    throw error
+  }
+}
 
 // This is temporary until @types/react-dom is updated
 export type InvoiceFormState = {
@@ -20,10 +35,9 @@ const InvoiceFormSchema = z.object({
     required_error: 'Please select a customer.',
     invalid_type_error: 'Please select a customer.',
   }),
-  amount: z.coerce
-  .number()
-  .gt(0, { message: 'Please enter an amount greater than $0.' })
-  .transform((amount) => amount * 100),
+  amount: z.coerce.number().
+    gt(0, { message: 'Please enter an amount greater than $0.' }).
+    transform((amount) => amount * 100),
   status: z.enum(['pending', 'paid'], {
     required_error: 'Please select an invoice status.',
     invalid_type_error: 'Please select an invoice status.',
@@ -34,8 +48,9 @@ const InvoiceFormSchema = z.object({
 const CreateInvoiceSchema = InvoiceFormSchema.omit({ id: true, date: true })
 const UpdateInvoiceSchema = InvoiceFormSchema.omit({ id: true, date: true })
 
-export async function createInvoice (prevState: InvoiceFormState, formData: FormData) {
-  console.log('-> createInvoice', Object.fromEntries(formData.entries()))
+export async function createInvoice (
+  prevState: InvoiceFormState, formData: FormData) {
+  console.log('-> createInvoice', Object.fromEntries(formData))
 
   const validatedFields = CreateInvoiceSchema.safeParse(
     Object.fromEntries(formData.entries()))
@@ -45,11 +60,11 @@ export async function createInvoice (prevState: InvoiceFormState, formData: Form
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Create Invoice.',
-    };
+    }
   }
 
   // Prepare data for insertion into the database
-  const { customerId, amount, status } = validatedFields.data;
+  const { customerId, amount, status } = validatedFields.data
   const date = new Date().toISOString().split('T')[0]
 
   try {
@@ -69,8 +84,9 @@ export async function createInvoice (prevState: InvoiceFormState, formData: Form
   redirect('/dashboard/invoices')
 }
 
-export async function updateInvoice (id: string, prevState: InvoiceFormState, formData: FormData) {
-  console.log('-> updateInvoice', id, Object.fromEntries(formData.entries()))
+export async function updateInvoice (
+  id: string, prevState: InvoiceFormState, formData: FormData) {
+  console.log('-> updateInvoice', id, Object.fromEntries(formData))
 
   const validatedFields = UpdateInvoiceSchema.safeParse(
     Object.fromEntries(formData.entries()))
@@ -80,11 +96,11 @@ export async function updateInvoice (id: string, prevState: InvoiceFormState, fo
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Update Invoice.',
-    };
+    }
   }
 
   // Prepare data for insertion into the database
-  const { customerId, amount, status } = validatedFields.data;
+  const { customerId, amount, status } = validatedFields.data
 
   try {
     await sql`
@@ -102,7 +118,7 @@ export async function updateInvoice (id: string, prevState: InvoiceFormState, fo
 }
 
 export async function deleteInvoice (id: string, formData: FormData) {
-  console.log('-> deleteInvoice', id, Object.fromEntries(formData.entries()))
+  console.log('-> deleteInvoice', id, Object.fromEntries(formData))
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '../acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-// import { signOut } from '@/auth';
+import { signOut } from '@/auth';
 
 export default function SideNav() {
   return (
@@ -21,8 +21,7 @@ export default function SideNav() {
         <form
           action={async () => {
             'use server';
-            console.log("Action signOut")
-            // await signOut();
+            await signOut();
           }}
         >
           <button className="flex h-[48px] w-full items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
